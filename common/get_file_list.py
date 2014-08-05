11f7eeb6879e432939d0dcd2e02409a85c96c802
Vladimir <Vladimir@localhost>
2014-08-05 16:24:40 -0700

    bad files are automatically removed now.

--- common/get_file_list.py
+++ common/get_file_list.py
@@ -25,14 +25,19 @@ def get_filelist_temp(modelName, path):
 
 def helper_function(fName):
   toOpen_indep = open(fName)
-  p = parser.Parser(toOpen_indep.read())
+  p = parser.Parser(toOpen_indep.read())  
+  try:
+    p.get_rho()
+  except:
+    print 'bad file = ', fName
+    os.remove(fName)
   toOpen_indep.close()
   return p
 
 
 def get_filelist(modelName, path):
   # return pool.map_async(helper_function, get_filelist_temp(modelName, path))
-  # return map(helper_function, get_filelist_temp(modelName, path))
+  return map(helper_function, get_filelist_temp(modelName, path))
   # return Parallel(n_jobs=3)(delayed(helper_function)(tx) for tx in get_filelist_temp(modelName, path))
-  return Parallel()(delayed(helper_function)(tx) for tx in get_filelist_temp(modelName, path))
+  # return Parallel()(delayed(helper_function)(tx) for tx in get_filelist_temp(modelName, path))
 
